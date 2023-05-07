51ff7b5a0c36b64288158bac90ca9691ee2c51c1
Villy Siu <villysiu@gmail.com>
2023-05-06 21:07:18 -0700

    Time: 55 ms (88.27%), Space: 42.6 MB (43.57%) - LeetHub

---
+++ 0014-longest-common-prefix/0014-longest-common-prefix.js
@@ -0,0 +1,15 @@
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+var longestCommonPrefix = function(strs) {
+    strs.sort()
+    // console.log(strs)
+    for (let i = 0; i < strs[0].length; i++) {
+        if (strs[0][i] !== strs[strs.length - 1][i]){
+            return strs[0].substr(0, i);
+        } 
+      }
+
+  return strs[0]; 
+};
