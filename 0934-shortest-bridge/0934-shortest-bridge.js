ba5da5a886ca2fbef51a5c728d95db24bda7b61d
Villy Siu <villysiu@gmail.com>
2023-05-21 11:24:03 -0700

    Time: 123 ms (52.04%), Space: 49.6 MB (78.57%) - LeetHub

---
+++ 0934-shortest-bridge/0934-shortest-bridge.js
@@ -0,0 +1,60 @@
+/**
+ * @param {number[][]} grid
+ * @return {number}
+ */
+var shortestBridge = function(grid) {
+    let n=grid.length
+    function findFirst(){
+        for(let i=0;i<n;i++){
+            for(let j=0;j<n;j++){
+                if(grid[i][j]===1)
+                    return [i,j]
+            }
+        }
+        return [null, null]
+    }
+
+    let dirs=[[0,1],[1,0],[-1,0],[0,-1]]
+    let queue=[]
+    function markIsland(r,c){
+        for(let d of dirs){
+            let R=d[0]+r, C=d[1]+c
+            if(R<0 || C<0 || R===n || C===n) continue
+            if(grid[R][C]===1){
+                grid[R][C]=2
+                queue.unshift([R,C])
+                markIsland(R,C)
+            }
+        }
+    }
+    let [r,c] = findFirst()
+    queue=[[r,c]]
+    grid[r][c]=2
+    markIsland(r,c)
+    
+    // console.log(queue)
+    // console.log(grid)
+    let visited=new Set()
+    let found=0
+    
+    function expand(){
+        while(queue.length>0){
+            let [r,c]=queue.shift()
+            visited.add(`${r}~${c}`)
+            for(let d of dirs){
+                let R=d[0]+r, C=d[1]+c
+                if(R<0 || C<0 || R===n || C===n || visited.has(`${R}~${C}`)) continue;
+                if(grid[R][C]===1){    
+
+                    return grid[r][c]
+                }
+                if(grid[R][C]<grid[r][c]){
+                    grid[R][C]=grid[r][c]+1
+                    queue.push([R,C])
+                }
+            }
+        }
+        return 0
+    }
+    return expand()-2
+};
