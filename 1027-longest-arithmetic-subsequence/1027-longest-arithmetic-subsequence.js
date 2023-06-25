06a7e8afd0e86eb57d4834cb2183c5a9492d6d77
Villy Siu <villysiu@gmail.com>
2023-06-24 21:45:29 -0700

    Time: 730 ms (74.53%), Space: 101.1 MB (23.85%) - LeetHub

--- 1027-longest-arithmetic-subsequence/1027-longest-arithmetic-subsequence.js
+++ 1027-longest-arithmetic-subsequence/1027-longest-arithmetic-subsequence.js
@@ -3,21 +3,17 @@
  * @return {number}
  */
 var longestArithSeqLength = function(nums) {
-    const n = nums.length;
-    if(n<=2) return n;
-
-    let longest = 2;
-    dp = Array(n).fill().map(() => new Map()) 
-
-    for(let i=0; i<n; ++i) {
-        for(let j=0; j<i; ++j) {
-            const diff = nums[i] - nums[j];
-            dp[i].set(diff, (dp[j].get(diff) || 1) + 1);
-
-            
-            longest = Math.max(longest, dp[i].get(diff));
+    let n = nums.length
+    let arr = Array.from({length: n}, ()=>new Map())
+    let res=0
+    for(let i=n-2;i>=0;i--){
+        for(let j=n-1;j>i;j--){
+            let diff = nums[i]-nums[j]
+            let count = arr[j].has(diff) ? arr[j].get(diff)+1 : 2
+            arr[i].set(diff, count)
+            res=Math.max(res, count)
         }
     }
-    // console.log(dp)
-    return longest;
+    // console.log(arr)
+    return res
 };
