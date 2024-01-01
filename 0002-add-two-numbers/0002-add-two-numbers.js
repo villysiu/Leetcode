d8f13b3fe383764d7d0d5d5f668d2cf8bddde541
Villy Siu <villysiu@gmail.com>
2023-12-31 22:30:27 -0800

    Time: 151 ms (9.77%), Space: 47.7 MB (30.53%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.js
+++ 0002-add-two-numbers/0002-add-two-numbers.js
@@ -14,12 +14,20 @@ var addTwoNumbers = function(l1, l2) {
     let res = new ListNode()
     let sum = 0, carry = 0
     let p=res
+    
     while(l1!==null || l2!==null) {
-        sum = (l1 ? l1.val: 0) + (l2 ? l2.val: 0) + carry
+        let val1=0,val2=0
+        if(l1){
+            val1=l1.val
+            l1 = l1.next
+        }
+        if(l2){
+            val2=l2.val
+            l2 = l2.next
+        }
+        sum = val1 + val2 + carry
         carry =  parseInt(sum/10)
         res.next = new ListNode(sum%10)
-        l1 = l1  ? l1.next : null
-        l2 = l2 ? l2.next : null
         res = res.next
     }
     if(carry)
