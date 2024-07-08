c64013565f7eb762eec4afb1680a14a84ea0b302
Villy Siu <villysiu@gmail.com>
2024-07-07 22:45:01 -0700

    Time: 70 ms (44.83%), Space: 56.5 MB (8.28%) - LeetHub

---
+++ 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.js
@@ -0,0 +1,46 @@
+/**
+ * @param {number} n
+ * @param {number} k
+ * @return {number}
+ */
+var findTheWinner = function(n, k) {
+    
+    // 20
+    // 6
+    // [ 9, 13, 16, 20, 1 ]
+    // 9 13 16 20 1 
+    // 9 13 16 20 1 
+    // 13 16 20 1
+    // 20 1 13 
+    // 20 1 
+    // 20
+    
+    //3
+    // 1
+    // [1,2,3]
+    // 1 2 3
+    // 2 3
+    // 3
+    if(k===1) return n
+    
+    let arr = new Array(n)
+    for(let i=0;i<n;i++)
+        arr[i]=i+1
+    
+    while(arr.length >= k ){
+        arr=[...arr.slice(k), ...arr.slice(0,k-1)]
+        
+    }
+    
+    while(arr.length>1){
+        let d = k%arr.length
+        
+        if(d===0)
+            arr = arr.slice(0,-1)
+        else
+            arr=[...arr.slice(d), ...arr.slice(0,d-1)]
+        
+    }
+    // console.log(arr)
+    return arr[0]
+};
