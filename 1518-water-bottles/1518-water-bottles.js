bf46e0543be7ed75c27c0dd961b1033de986ea5f
Villy Siu <villysiu@gmail.com>
2024-07-07 23:19:30 -0700

    Time: 52 ms (50.83%), Space: 48.8 MB (28.33%) - LeetHub

--- 1518-water-bottles/1518-water-bottles.js
+++ 1518-water-bottles/1518-water-bottles.js
@@ -4,7 +4,7 @@
  * @return {number}
  */
 var numWaterBottles = function(numBottles, numExchange) {
-    
+    if (numBottles < numExchange) return numBottles;
     let res = numBottles
     while(numBottles>=numExchange){
         let ex = parseInt(numBottles/numExchange)
