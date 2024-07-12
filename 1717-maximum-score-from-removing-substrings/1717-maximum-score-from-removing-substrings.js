d8fd183378ab314c66da838fd64d02f9c9752804
Villy Siu <villysiu@gmail.com>
2024-07-12 15:33:57 -0700

    Time: 193 ms (8.33%), Space: 72.7 MB (8.33%) - LeetHub

--- 1717-maximum-score-from-removing-substrings/1717-maximum-score-from-removing-substrings.js
+++ 1717-maximum-score-from-removing-substrings/1717-maximum-score-from-removing-substrings.js
@@ -8,15 +8,17 @@ var maximumGain = function(s, x, y) {
     // ababbaaxy
     // axy
     let stack=[]
-    let sorted_score=[[x, "ab"],[y,"ba"]].sort((a,b)=>b[0]-a[0])
+    // let sorted_score=[[x, "ab"],[y,"ba"]].sort((a,b)=>b[0]-a[0])
+    let sorted_score= x>y ? [[x, "ab"],[y,"ba"]] : [[y,"ba"],[x, "ab"]]
     let points = 0
     
     for(let [score, str] of sorted_score){
         for(let ch of s){
             // console.log(ch)
-            if(stack.length === 0)
-                stack.push(ch)
-            else if(ch === str[1] && stack[stack.length-1]===str[0]){
+            // if(stack.length === 0)
+            //     stack.push(ch)
+            // else 
+            if(stack.length >0 && ch === str[1] && stack[stack.length-1]===str[0]){
                 stack.pop()
                 points+=score
                 // console.log(points)
