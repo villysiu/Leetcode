f37efc7086648472b98f0875b6eb1f0a82f970e9
Villy Siu <villysiu@gmail.com>
2024-08-12 14:11:06 -0700

    Time: 295 ms (28.71%), Space: 61.2 MB (30.77%) - LeetHub

--- 0703-kth-largest-element-in-a-stream/0703-kth-largest-element-in-a-stream.js
+++ 0703-kth-largest-element-in-a-stream/0703-kth-largest-element-in-a-stream.js
@@ -3,8 +3,11 @@
  * @param {number[]} nums
  */
 var KthLargest = function(k, nums) {
-    this.arr=nums.sort((a,b)=>a-b)
-    this.k=k
+    // 8, 5,4,2  [3], [5], [10], [9], [4]
+    // 10, 9 8, 5,5 4 3 ,2
+    this.sortedArr = nums.sort((a,b)=>b-a).slice(0,k)
+    this.k = k
+    
     
 };
 
@@ -13,26 +16,46 @@ var KthLargest = function(k, nums) {
  * @return {number}
  */
 KthLargest.prototype.add = function(val) {
-    function insert_value(arr){
-        let l=0, r=arr.length-1
-        while(l<=r){
-            let m=parseInt((l+r)/2)
-            if(arr[m]===val){
-                return m
+    // this.sortedArr = [...this.sortedArr, val].sort((a,b)=>b-a)
 
-            }else if(arr[m]>val){
-                r=m-1
-            }else{
-                l=m+1
-            }
-
+    if(val < this.sortedArr[this.k-1])
+        return this.sortedArr[this.k-1]
+    
+    let l = 0, r=this.k-1
+    while(l<=r){
+        
+        let mid = parseInt((l+r)/2)
+        // console.log(l, r, mid)
+        if(val === this.sortedArr[mid]){
+            this.sortedArr = [...this.sortedArr.slice(0,mid), val, ...this.sortedArr.slice(mid, this.k-1)]
+            break;
+        }else if(val < this.sortedArr[mid]){
+            l=mid+1
+            
+            
+        }else{
+            r=mid-1
+        
         }
-        return l
     }
-    pos = insert_value(this.arr)
-    this.arr.splice(pos, 0, val)
-    // console.log(this.arr)
-    return this.arr[this.arr.length-this.k]
+    // 0 1 2 3 4 5 6 7
+    // 8 5 4
+    
+    // 0 2 1
+    // 2 2 2
+    // 3 2
+    // 
+    
+    if(l>r){
+        // console.log(this.sortedArr.slice(0,l))
+        // console.log(this.sortedArr.slice(l, this.k-1))
+        this.sortedArr = [...this.sortedArr.slice(0,l), val, ...this.sortedArr.slice(l, this.k-1)]
+    }
+    // console.log("end")
+    // console.log(this.sortedArr)
+    
+    
+    return this.sortedArr[this.k-1]
 };
 
 /** 
