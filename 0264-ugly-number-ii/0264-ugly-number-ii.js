42f90cdce38346e1bbecfeb98a590add210049fe
Villy Siu <villysiu@gmail.com>
2024-08-17 23:59:45 -0700

    Time: 64 ms (51.26%), Space: 50.8 MB (82.35%) - LeetHub

--- 0264-ugly-number-ii/0264-ugly-number-ii.js
+++ 0264-ugly-number-ii/0264-ugly-number-ii.js
@@ -3,45 +3,21 @@
  * @return {number}
  */
 var nthUglyNumber = function(n) {
-    let list = [1]
-    let twos =[] 
-    let threes = [] 
-    let fives = [] 
-    
-    
-
-    while(list.length<n){
-         
-        twos.push(list[list.length-1]*2)
-        threes.push(list[list.length-1]*3)
-        fives.push(list[list.length-1]*5)
-        // console.log(list)
-        // console.log(twos)
-        // console.log(threes)
-        // console.log(fives)
-        while(twos[0]<=list[list.length-1]){
-            twos.shift()
-        }
-        while(threes[0]<=list[list.length-1]){
-            threes.shift()
-        }
-        while(fives[0]<=list[list.length-1]){
-            fives.shift()
-        }
-        // console.log(twos[0], threes[0], fives[0])
-        if(twos[0]<=threes[0] && twos[0]<=fives[0]){
-            list.push(twos[0])
-            
-        }
-        else if(threes[0]<=twos[0] && threes[0]<=fives[0]){
-            list.push(threes[0])
-            
-        }
-        else if(fives[0]<=twos[0] && fives[0]<=threes[0]){
-            list.push(fives[0])
-           
-        }
+    let dp = new Array(n).fill(0)
+    dp[0] = 1
+    let p2 = 0
+    let p3 = 0
+    let p5 = 0
+    for(let i=1; i<n; i++){
+        
+        dp[i] = Math.min(dp[p2]*2, dp[p3]*3, dp[p5]*5)
+        if(dp[i] === dp[p2]*2)
+            p2++
+        if(dp[i] === dp[p3]*3)
+            p3++
+        if(dp[i] === dp[p5]*5)
+            p5++
         
     }
-    return list[list.length-1]
+    return dp[n-1]
 };
