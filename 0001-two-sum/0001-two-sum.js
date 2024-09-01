28c52f204cd8b5415ebbd377e9e916ae1a837b62
Villy Siu <villysiu@gmail.com>
2024-08-31 17:01:45 -0700

    Time: 63 ms (59.95%), Space: 53.9 MB (5.28%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -4,26 +4,30 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-    let hash = {}
+    let arr=[]
     for(let i=0; i<nums.length; i++){
-        
-        let n = nums[i]
-        
-        if(!hash[n])
-            hash[n] = []
-        hash[n].push(i)
+        arr.push([nums[i], i])
     }
-    for(let i=0; i<nums.length; i++){
-        let n = nums[i]
-        if(target-n === n){
-            if(hash[target - n].length===1)
-                continue;
-            
-            return [i, hash[target - n][1]]
-           
-                
+    arr.sort((a, b)=>{
+        return a[0]-b[0]
+    })
+    // console.log(arr)
+    
+    for(let i=0; i<arr.length; i++){
+        let [val, orid_idx] = arr[i]
+        let diff = target - val
+        
+        let l = i+1, r = arr.length-1
+        while(l<=r){
+            let m = parseInt((l+r)/2)
+            if(arr[m][0] === diff){
+                return [arr[i][1], arr[m][1]]
+            }
+            else if(arr[m][0] > diff){
+                r = m - 1
+            }
+            else
+                l = m + 1
         }
-        else if(hash[target - n])
-            return [i, hash[target - n][0]]
     }
 };
