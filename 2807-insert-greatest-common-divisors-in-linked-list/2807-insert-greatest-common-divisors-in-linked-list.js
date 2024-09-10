df5067cffd074fe3eb0690a28c371ace70ddebca
Villy Siu <villysiu@gmail.com>
2024-09-09 22:13:55 -0700

    Time: 94 ms (70.37%), Space: 58.4 MB (50.26%) - LeetHub

---
+++ 2807-insert-greatest-common-divisors-in-linked-list/2807-insert-greatest-common-divisors-in-linked-list.js
@@ -0,0 +1,33 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var insertGreatestCommonDivisors = function(head) {
+    
+    const GCD = (a,b)=>{
+        if(a % b === 0) 
+            return b;
+        
+        return GCD(b, a % b)
+        
+    }
+   // console.log(GCD(6,10))
+    let p = head
+    while(p && p.next){
+        // console.log(p.val, p.next.val)
+        let node = new ListNode(GCD(p.val, p.next.val), p.next)
+        
+        p.next = node
+        p=p.next.next
+        
+    }
+    return head
+    
+};
