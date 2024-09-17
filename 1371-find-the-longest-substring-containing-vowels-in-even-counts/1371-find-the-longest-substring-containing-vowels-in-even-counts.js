452312a88e73c39161ab2d5bc86816fae759ddc1
Villy Siu <villysiu@gmail.com>
2024-09-17 10:12:12 -0700

    Time: 124 ms (12.55%), Space: 56.9 MB (15.34%) - LeetHub

--- 1371-find-the-longest-substring-containing-vowels-in-even-counts/1371-find-the-longest-substring-containing-vowels-in-even-counts.js
+++ 1371-find-the-longest-substring-containing-vowels-in-even-counts/1371-find-the-longest-substring-containing-vowels-in-even-counts.js
@@ -4,9 +4,11 @@
  */
 var findTheLongestSubstring = function(s) {
     const n = s.length
-    const vowelsObj = {a: 0, e: 1, i: 2, o: 3, u: 4}
-    
-    // let prefixObj = {}
+    const kvArray = [
+  ["key1", "value1"],
+  ["key2", "value2"],
+];
+    const vowelsMap = new Map([["a", 0], ["e", 1], ["i", 2], ["o", 3],["u", 4]])
     let prefixMap = new Map()
     
     let str = "00000"
@@ -16,8 +18,8 @@ var findTheLongestSubstring = function(s) {
     for(let i=0; i<n; i++){
         let letter = s[i]
             
-        if(vowelsObj[letter]!==undefined){      
-            let pos = vowelsObj[letter]
+        if(vowelsMap.has(letter)){      
+            let pos = vowelsMap.get(letter)
             str = str.slice(0,pos) + (str[pos] === "1"? "0" : "1") + str.slice(pos+1)
 
             }
