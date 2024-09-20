133111b52fb87e63225c3c6de4fca67e04d0f643
Villy Siu <villysiu@gmail.com>
2024-09-20 09:11:59 -0700

    Time: 63 ms (22.62%), Space: 51.8 MB (26.79%) - LeetHub

--- 0241-different-ways-to-add-parentheses/0241-different-ways-to-add-parentheses.js
+++ 0241-different-ways-to-add-parentheses/0241-different-ways-to-add-parentheses.js
@@ -3,28 +3,22 @@
  * @return {number[]}
  */
 var diffWaysToCompute = function(expression) {
-    const regexp = /(\d{1,2})|([\+\-\*])/g
-    const arr = expression.match(regexp);
     
-    // console.log(arr); 
-// "2-1-1*6+1"
-    const helper = (arr) => {
-        
-        if(arr.length===1)
-            return [parseInt(arr[0])]
+    const helper = (exp) => {
+    
         let res = []
-        for(let i=0; i<arr.length; i++){
+        for(let i=0; i<exp.length; i++){
             
-            if(arr[i]==="+" || arr[i]==="-" || arr[i]==="*"){
-                let leftArr = helper(arr.slice(0, i)) 
-                let rightArr = helper(arr.slice(i+1))
+            if(exp[i]==="+" || exp[i]==="-" || exp[i]==="*"){
+                let leftArr = helper(exp.slice(0, i)) 
+                let rightArr = helper(exp.slice(i+1))
                 // console.log(leftArr, rightArr)
                 
                 for(let m of leftArr){
                     for(let n of rightArr){
-                        if(arr[i] === "+")
+                        if(exp[i] === "+")
                             res.push(m+n)
-                        else if(arr[i] === "-")
+                        else if(exp[i] === "-")
                             res.push(m-n)
                         else 
                             res.push(m*n)
@@ -34,10 +28,12 @@ var diffWaysToCompute = function(expression) {
             
         }
         // console.log(res)
+        if(res.length===0)
+            return [parseInt(exp)]
         return res
     }
     
-    return helper(arr)
+    return helper(expression)
     
     
 };
