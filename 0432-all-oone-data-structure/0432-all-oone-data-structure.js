e6b4451795bcc89a8d0f7b4f2b71a8135fc98068
Villy Siu <villysiu@gmail.com>
2024-09-30 10:57:30 -0700

    Time: 438 ms (10.13%), Space: 72 MB (79.75%) - LeetHub

--- 0432-all-oone-data-structure/0432-all-oone-data-structure.js
+++ 0432-all-oone-data-structure/0432-all-oone-data-structure.js
@@ -2,6 +2,23 @@
 var AllOne = function() {
     this.keyCount = new Map()
     this.countKeys = new Map()
+    
+    this.helper = (from, to, key) =>{
+
+        if(to >= 1){
+            if(!this.countKeys.has(to)){
+                this.countKeys.set(to, new Set())
+            }
+            this.countKeys.get(to).add(key)
+        }
+        
+        if(from >= 1){
+            this.countKeys.get(from).delete(key)
+            if(this.countKeys.get(from).size === 0)
+                this.countKeys.delete(from)
+        }
+        
+    }
 };
 
 /** 
@@ -13,21 +30,7 @@ AllOne.prototype.inc = function(key) {
     let count = (!this.keyCount.has(key)) ? 1 : this.keyCount.get(key)+1
     
     this.keyCount.set(key, count)
-    
-    if(!this.countKeys.has(count)){
-        this.countKeys.set(count, new Set())
-        
-    }
-    this.countKeys.get(count).add(key)
-    if(count > 1){
-        this.countKeys.get(count-1).delete(key)
-        if(this.countKeys.get(count-1).size === 0){
-            this.countKeys.delete(count-1)
-        }
-    }
-        
-    
-
+    this.helper(count-1, count, key)
 };
 
 /** 
@@ -36,24 +39,14 @@ AllOne.prototype.inc = function(key) {
  */
 AllOne.prototype.dec = function(key) {
     let count = this.keyCount.get(key)
+    
     if(count===1){
         this.keyCount.delete(key)
-        this.countKeys.get(1).delete(key)
-        if(this.countKeys.get(1).size===0)
-            this.countKeys.delete(1)
     }
     else{
         this.keyCount.set(key, count-1)
-        this.countKeys.get(count).delete(key)
-        if(this.countKeys.get(count).size === 0)
-            this.countKeys.delete(count)
-        if(!this.countKeys.has(count-1))
-            this.countKeys.set(count-1, new Set())
-        this.countKeys.get(count-1).add(key)
     }
-        
-    
-    
+    this.helper(count, count-1, key)
 };
 
 /**
@@ -64,8 +57,6 @@ AllOne.prototype.getMaxKey = function() {
     if(this.countKeys.size === 0)
         return ""
     let max = Math.max(...this.countKeys.keys())
-    // console.log(max)
-    // console.log([...this.countKeys.get(max)])
     return [...this.countKeys.get(max)][0]
 };
 
@@ -73,11 +64,10 @@ AllOne.prototype.getMaxKey = function() {
  * @return {string}
  */
 AllOne.prototype.getMinKey = function() {
+    // console.log(this.countKeys)
     if(this.countKeys.size === 0)
         return ""
     let min = Math.min(...this.countKeys.keys())
-    // console.log(min)
-    // console.log([...this.countKeys.get(min)])
     return [...this.countKeys.get(min)][0]
 };
 
