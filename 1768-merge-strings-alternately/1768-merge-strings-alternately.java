ad711dde37a24e7f1f46b3f0ef936c5c0c9ef440
Villy Siu <villysiu@gmail.com>
2024-11-14 16:11:34 -0800

    Time: 0 ms (100.00%), Space: 41.7 MB (61.40%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.java
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.java
@@ -1,12 +1,31 @@
 class Solution {
     public String mergeAlternately(String word1, String word2) {
         
-        int commonLen = (word1.length() < word2.length()) ? word1.length() : word2.length();
-        String res = "";
-        for(int i=0; i<commonLen; i++){
-            res = res + word1.charAt(i) + word2.charAt(i);
+        if(word1.length() == 0){
+            return word2;
+        }else if(word2.length() == 0){
+            return word1;
         }
-        res += word1.substring(commonLen) + word2.substring(commonLen);
-        return res;
+
+        StringBuilder finalWord = new StringBuilder();
+        int len1 = word1.length();
+        int len2 = word2.length();
+
+        if(len1 > len2){
+            for(int i = 0 ,  j = 0 ; i < len2 ; i++ , j++){
+                finalWord.append(word1.charAt(i));
+                finalWord.append(word2.charAt(j));
+            }
+            finalWord.append(word1.substring(len2));
+        }else 
+        {
+            for(int i = 0 ,  j = 0 ; i < len1 ; i++ , j++){
+                finalWord.append(word1.charAt(i));
+                finalWord.append(word2.charAt(j));
+            }
+            finalWord.append(word2.substring(len1));
+        }
+
+        return finalWord.toString();
     }
 }
