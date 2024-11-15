c4d59cb61a8786795f0f04609e1770784af99be1
Villy Siu <villysiu@gmail.com>
2024-11-14 17:06:31 -0800

    Time: 0 ms (100.00%), Space: 41.8 MB (40.16%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.java
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.java
@@ -1,31 +1,27 @@
 class Solution {
     public String mergeAlternately(String word1, String word2) {
-        
-        if(word1.length() == 0){
+        if(word1.length() == 0)
             return word2;
-        }else if(word2.length() == 0){
+        if(word2.length() == 0)
             return word1;
-        }
-
-        StringBuilder finalWord = new StringBuilder();
-        int len1 = word1.length();
-        int len2 = word2.length();
-
-        if(len1 > len2){
-            for(int i = 0 ,  j = 0 ; i < len2 ; i++ , j++){
-                finalWord.append(word1.charAt(i));
-                finalWord.append(word2.charAt(j));
+            
+        StringBuilder res = new StringBuilder();
+        if(word1.length() < word2.length()){
+            for(int i = 0; i<word1.length(); i++){
+                res.append(word1.charAt(i));
+                res.append(word2.charAt(i));
             }
-            finalWord.append(word1.substring(len2));
-        }else 
-        {
-            for(int i = 0 ,  j = 0 ; i < len1 ; i++ , j++){
-                finalWord.append(word1.charAt(i));
-                finalWord.append(word2.charAt(j));
-            }
-            finalWord.append(word2.substring(len1));
+            
+            res.append(word2.substring(word1.length()));
         }
-
-        return finalWord.toString();
+        else {
+             for(int i = 0; i<word2.length(); i++){
+                res.append(word1.charAt(i));
+                res.append(word2.charAt(i));
+            }
+            
+            res.append(word1.substring(word2.length()));
+        }
+        return res.toString();
     }
 }
